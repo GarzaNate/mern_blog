b1c2f2d992fddd9a6092617ab46f88e719b21ee0
slendyG <josuegarza0608@gmail.com>
2023-01-17 00:23:43 -0800

    added middleware packages to server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileUrlToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import projectRoutes from "./routes/projects.js";
 
 // CONFIGURATION
-const app = express();
+const __filename = fileUrlToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
-app.use(express.urlencoded({ extended: true }));
+const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+// FILE STORAGE
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    file: function (req, file, cb) {
+        cb(null, file.originalname);
+}
+});
 
 // ROUTES
 app.use("/auth", authRoutes);
