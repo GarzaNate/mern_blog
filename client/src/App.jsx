55515d28b963000d35a7dc69225dbd5d0da7b719
slendyG <josuegarza0608@gmail.com>
2024-03-22 23:59:37 -0700

    added header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import Home from "./pages/home";
 import SignUp from "./pages/signUp";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
+import Header from "./components/header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/sign-up" element={ <SignUp /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
