d28e1fce4cd95d1d51cd4e766ff1802f8336a30c
slendyG <josuegarza0608@gmail.com>
2024-06-22 00:23:25 -0700

    added react-icons and made changes to header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,15 +1,64 @@
 import { Link } from "react-router-dom";
-
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 function Header() {
   return (
-    <div className="bg-primary text-secondaryW">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
-        {/* HEADER TITLE */}
-        <h1>Welcome to the MERN blog!</h1>
+    <div>
+      {/* NAVBAR */}
+      <Navbar className="border-b-2">
+        <Link
+          to={"/"}
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white rounded-lg">
+            MERN
+          </span>{" "}
+          BLOG
+        </Link>
 
+        {/* possible search function */}
+        <form>
+          <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={AiOutlineSearch}
+            className="hidden lg:inline"
+          />
+        </form>
+        {/* Search Button */}
+        <Button className="w-12 h-10 lg:hidden" color="gray" pill>
+          <AiOutlineSearch />
+        </Button>
+
+        <div className="flex gap-2 md:order-2">
+          {/* Dark mode toggle */}
+          <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Link to={"/login"}>
+            <Button gradientDuoTone='purpleToBlue' >
+              Login
+            </Button>
+          </Link>
+
+          <Navbar.Collapse>
+            <Navbar.Link>
+              <Link to={"/"}>
+                Home
+              </Link>
+            </Navbar.Link>
+            <Navbar.Link>
+              <Link to={"/sign-up"}>
+                Sign Up
+              </Link>
+            </Navbar.Link>
+            
+          </Navbar.Collapse>
+        </div>
         {/* NAV LIST */}
-        <ul className="flex gap-4">
-          <Link to={"/"}>
+        {/* <ul className="flex gap-4">
+          <Link to={'/'}>
             <li>Home</li>
           </Link>
           <Link to={"/sign-up"}>
@@ -18,8 +67,8 @@ function Header() {
           <Link to={"/login"}>
             <li>Login</li>
           </Link>
-        </ul>
-      </div>
+        </ul> */}
+      </Navbar>
     </div>
   );
 }
